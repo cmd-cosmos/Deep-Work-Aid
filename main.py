f820b3acc59231a123138222dcfbcdd6a0380dbb
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-28 14:07:03 -0500

    added a job class to classify input jobs.

--- main.py
+++ main.py
@@ -4,8 +4,29 @@
 import time
 import os
 import psutil
+from enum import Enum
 
+job_list = []
 distractions = ["chrome.exe", "Chrome.exe", "brave.exe"]
+
+
+class Job_Nature(Enum):
+    WORK = 0
+    DISTRACTION = 1
+
+class Job:
+    def __init__(self,id, type, time) -> None:
+        self.id = id
+        self.type = type
+        self.time = time
+    
+    def categorize(self):
+        if self.type == Job_Nature.DISTRACTION:
+            print("Distracting Process Detected")
+            job_list.append(id)
+        elif self.type == Job_Nature.WORK:
+            print("Setting as target job.")
+
 
 for proc in psutil.process_iter(['pid', 'name']):
     if proc.info['name'] in distractions:
