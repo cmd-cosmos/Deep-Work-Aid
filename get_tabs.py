8f66d7525170dc46f6c86419e6197f886a8b0f8a
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-29 13:56:50 -0500

    pdf process getter function added, not functional yet.

--- get_tabs.py
+++ get_tabs.py
@@ -1,12 +1,30 @@
 #pylint: skip-file
 #type: ignore
 
+import os
 import subprocess
 
-subprocess.Popen([
-    r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe",
-    "--remote-debugging-port=9222",
-    r'--user-data-dir=C:\BraveDebug',
-    "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgLLlzdgiTUKULKJPYc0A4q"
-    ])
+target_lectures = {
+        "computer architecture" : "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgLLlzdgiTUKULKJPYc0A4q" 
+}
 
+target_readings = {
+    "computer memory" : r"C:\Users\premt\OneDrive\Desktop\Programming Books and Notes\NOTES - AND - TEXTBOOKS\ESSENTIAL PAPERS\script.bat",
+
+
+}
+
+def brave_process_getter():
+    subprocess.Popen([
+        r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe",
+        "--remote-debugging-port=9222",
+        r'--user-data-dir=C:\BraveDebug',
+        target_lectures["computer architecture"]
+        ])
+
+def pdf_process_getter():
+    os.system(f"start {target_readings["computer memory"]}")
+
+if __name__ == "__main__":
+    brave_process_getter()
+    pdf_process_getter()
